5e341421df97f7d11166218b524e338faa199376
Lewis Wheeler <Web-Slinger@outlook.com>
2020-11-19 18:21:14 +0000

    App.config for Mongo DBNAME, Mongo URI & SECRET_KEY added to run.py.

--- run.py
+++ run.py
@@ -9,6 +9,11 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
