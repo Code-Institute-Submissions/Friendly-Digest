271192b281de110c933a9d5e40bffae7becfc8b8
Lewis Wheeler <Web-Slinger@outlook.com>
2020-12-19 21:16:06 +0000

    Reformatted script.js file. Removed unnecessary code.

--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,32 @@
+$(document).ready(function () {
+// Side nav for mobile view
+  $(".button-collapse").sideNav();
+
+// Clickable recipes dropdown menu for mobile side-nav
+  $('.dropdown-trigger').dropdown();
+
 // Hoverable recipes dropdown menu for desktop
-$('.dropdown-button').dropdown({
+  $('.dropdown-button').dropdown({
   inDuration: 400,
   outDuration: 225, 
   hover: true,
-  }
-);
+  });
 
-// Side nav for mobile view
-$(".button-collapse").sideNav();
-
-// Clickable recipes dropdown menu for mobile side-nav
-$(document).ready(function(){
-  $('.dropdown-trigger').dropdown();
-});
-
-// Copyright date updated every year
-$("#copyright").text(new Date().getFullYear());
-
-// Carousel: Image animation that scrolls through different images
-$(document).ready(function () {
+// Image animation that scrolls through different images
   $('.carousel').carousel();
   setInterval(function () {
     $('.carousel').carousel('next');
-  }, 3000)
-});
+  }, 3000);
 
 // Clickable images to make full screen with message
-$(document).ready(function(){
   $('.materialboxed').materialbox();
-});
-
-// Modal to appear when success story images are clicked
-$(document).ready(function(){
-  $('.modal').modal();
-});
 
 // Tooltip messages for Success Stories
-$(document).ready(function(){
   $('.tooltipped').tooltip({delay: 50});
+
+// Modal to appear when success story images are clicked
+  $('.modal').modal();
+
+// Copyright date updated every year
+  $("#copyright").text(new Date().getFullYear());
 });
